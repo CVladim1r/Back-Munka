dcc695832fe05540c6e17907bcc1c3dc353c44be
CVladim1r <vladimir.973@list.ru>
2024-05-16 17:25:55 +0300

    swagger-update

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+# Running the backend purely on flask
 from app import create_app
 
 app = create_app()
--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,4 @@
+# Running the backend with gunicorn
 # gunicorn -w 4 wsgi:app
 from app import create_app
 
